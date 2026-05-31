76c070397023c294f37aad39f06136cab9b8487e
Deepamjyoti Mohanty <deepamjyoti12345@gmail.com>
2026-05-31 17:48:42 +0530

    debug inbox

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -43,6 +43,8 @@ type ApiSession = {
 };
 
 const apiBase = (import.meta.env.VITE_API_URL ?? "").replace(/\/$/, "");
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("apiBase =", apiBase);
 
 export type EmailAccount = {
   id: string;
